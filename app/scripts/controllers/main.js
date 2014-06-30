165b8c0b33e79d249e0e258cb4b9178c9948c13b
OBipolar <shaohuixu13@gatech.edu>
2014-06-30 11:16:30 +0800

    modify the stacked chart

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,21 @@
 angular.module('angularReportChartApp')
   .controller('MainCtrl', function ($scope, $http) {
 
+        var colorArray = ['#389C75', '#FEFEFE', '#DFD122', '#3668A4', '#753790'];
+
         $http.get('lineChartData.json').success(function(data) {
-          $scope.lineChartData = data ;
+          $scope.influxData = data ;
         });
 
-            $scope.xAxisTickFormatFunction = function() {
+        $scope.xAxisTickFormatFunction = function() {
                 return function(d) {
             return d3.time.format('%x')(new Date(d));
           };
-              };
+        };
+
+        $scope.colorFunction = function() {
+                return function(d, i) {
+                  return colorArray[i];
+                }
+        }
   });
